57b73441e1e9e351d28948d9964f062be9bb6173
Sangwoo Im <saim80@me.com>
2023-08-09 10:43:09 -0400

    Project intialized

---
+++ Source/FineWidget/Public/FineBaseWidget.h
@@ -0,0 +1,17 @@
+// (c) 2023 Pururum LLC. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UObject/Object.h"
+#include "FineBaseWidget.generated.h"
+
+/**
+ * Base class for all user widgets in FineWidget. 
+ */
+UCLASS()
+class FINEWIDGET_API UFineBaseWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
