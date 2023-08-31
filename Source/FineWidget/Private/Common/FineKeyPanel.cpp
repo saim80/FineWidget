9b06684cfadab484a047097247d4c1b2b299e331
Sangwoo Im <saim80@me.com>
2023-08-31 18:41:50 -0400

    Button Key panel widget added.

---
+++ Source/FineWidget/Private/Common/FineKeyPanel.cpp
@@ -0,0 +1,69 @@
+// (c) 2023 Pururum LLC. All rights reserved.
+
+
+#include "Common/FineKeyPanel.h"
+
+#include "Components/Image.h"
+#include "Components/Spacer.h"
+#include "Components/TextBlock.h"
+
+void UFineKeyPanel::SetKey(const FKey& NewKey)
+{
+	if (Key != NewKey)
+	{
+		Key = NewKey;
+		UpdateKeyPresentation();
+	}
+}
+
+void UFineKeyPanel::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	UpdateKeyPresentation();
+}
+
+void UFineKeyPanel::NativeConstruct()
+{
+	Super::NativeConstruct();
+	UpdateKeyPresentation();
+}
+
+void UFineKeyPanel::UpdateKeyPresentation()
+{
+	UpdatePresentationDataFromKey();
+	if (IsValid(KeySpacer))
+	{
+		if (!KeyTextValue.IsEmpty() && !KeyIconTexture.IsNull())
+		{
+			KeySpacer->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		else
+		{
+			KeySpacer->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
+	if (IsValid(KeyText))
+	{
+		if (!KeyTextValue.IsEmpty())
+		{
+			KeyText->SetText(KeyTextValue);
+			KeyText->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		else
+		{
+			KeyText->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
+	if (IsValid(KeyImage))
+	{
+		if (!KeyIconTexture.IsNull())
+		{
+			KeyImage->SetBrushFromSoftTexture(KeyIconTexture);
+			KeyImage->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		else
+		{
+			KeyImage->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
+}
---
+++ Source/FineWidget/Public/Common/FineKeyPanel.h
@@ -0,0 +1,53 @@
+// (c) 2023 Pururum LLC. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FineBaseWidget.h"
+#include "InputCoreTypes.h"
+#include "UObject/Object.h"
+#include "FineKeyPanel.generated.h"
+
+class USpacer;
+class UImage;
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class FINEWIDGET_API UFineKeyPanel : public UFineBaseWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetKey(const FKey& NewKey);
+	FORCEINLINE const FKey& GetKey() const { return Key; }
+
+protected:
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void UpdatePresentationDataFromKey();
+	virtual void UpdateKeyPresentation();
+
+private:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "FineKeyPanel", meta = (AllowPrivateAccess = "true"))
+	FKey Key;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FineKeyPanel", meta = (AllowPrivateAccess = "true"))
+	TSoftObjectPtr<UTexture2D> KeyIconTexture;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FineKeyPanel", meta = (AllowPrivateAccess = "true"))
+	FText KeyTextValue;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "FineKeyPanel",
+		meta = (AllowPrivateAccess = "true", BindWidgetOptional))
+	UTextBlock* KeyText;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "FineKeyPanel",
+		meta = (AllowPrivateAccess = "true", BindWidgetOptional))
+	USpacer* KeySpacer;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "FineKeyPanel",
+		meta = (AllowPrivateAccess = "true", BindWidgetOptional))
+	UImage* KeyImage;
+};
